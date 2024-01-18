72dbb7e316139359016cac20a38bef28c4f05c8b
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-01-18 14:20:30 +0545

    udpates

--- laravelapp/resources/views/welcome.blade.php
+++ laravelapp/resources/views/welcome.blade.php
@@ -16,11 +16,17 @@
         </style>
     </head>
     <body class="antialiased">
+        <iframe width="724" height="407" src="https://www.youtube.com/embed/ta07bMHe8Lk" title="Teenpatey - Dekhera Timilai | Sujan Chapagain&amp;Bidhya Tiwari Ft. Milan|Anu|Samrant|Hark|Nayan|Ayushma" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+
+
+
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
