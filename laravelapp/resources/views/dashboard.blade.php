51ad8f8d180023f8176d2eed6547c74043aa5d78
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-01-22 14:55:52 +0545

    updates

--- laravelapp/resources/views/dashboard.blade.php
+++ laravelapp/resources/views/dashboard.blade.php
@@ -1,4 +1,22 @@
 @extends('layouts.app')
 @section('content')
-    <p>This is dashboard</p>
+    <h1 class="text-3xl font-bold text-indigo-600">Dashboard</h1>
+    <hr class="my-2 h-1 bg-red-600">
+
+    <div class="grid grid-cols-3 gap-10">
+        <div class="bg-blue-600 rounded-tr-full text-white rounded-lg flex justify-between py-4 px-2">
+            <h1 class="text-xl">Total Blogs</h1>
+            <h1 class="text-4xl font-bold">10</h1>
+        </div>
+
+        <div class="bg-red-600 text-white rounded-lg flex justify-between py-4 px-2">
+            <h1 class="text-xl">Total Blogs</h1>
+            <h1 class="text-4xl font-bold">10</h1>
+        </div>
+
+        <div class="bg-green-600 text-white rounded-lg flex justify-between py-4 px-2">
+            <h1 class="text-xl">Total Blogs</h1>
+            <h1 class="text-4xl font-bold">10</h1>
+        </div>
+    </div>
 @endsection
--- laravelapp/resources/views/layouts/app.blade.php
+++ laravelapp/resources/views/layouts/app.blade.php
@@ -10,16 +10,26 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link
+            href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css"
+            rel="stylesheet"
+        />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="flex">
-            <div class="w-56 h-screen bg-gray-200 shadow">
+            <div class="w-56 h-screen bg-gray-100 shadow">
                 <img src="{{asset('images/logo.png')}}" class="mt-5 w-full" alt="">
-                <div>
-                    Menu here
+                <div class="mt-5 text-blue-900">
+                    <a href="" class="p-2 block hover:bg-blue-500 hover:text-white text-lg border-b border-gray-300"><i class="ri-dashboard-2-line"></i> Dashboard</a>
+                    <a href="" class="p-2 block hover:bg-blue-500 hover:text-white text-lg border-b border-gray-300"><i class="ri-image-2-line"></i> Banner</a>
+                    <a href="" class="p-2 block hover:bg-blue-500 hover:text-white text-lg border-b border-gray-300"><i class="ri-window-line"></i> Projects</a>
+                    <a href="" class="p-2 block hover:bg-blue-500 hover:text-white text-lg border-b border-gray-300"><i class="ri-news-line"></i> Blogs</a>
+                    <a href="" class="p-2 block hover:bg-blue-500 hover:text-white text-lg border-b border-gray-300"><i class="ri-contacts-book-line"></i> Contacts</a>
+                    <a href="" class="p-2 block hover:bg-blue-500 hover:text-white text-lg border-b border-gray-300"><i class="ri-logout-box-line"></i> Logout</a>
+
                 </div>
             </div>
             <div class="p-4 flex-1">
